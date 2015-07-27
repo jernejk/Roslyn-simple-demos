cbf75f740a4bc761527a49962e844bd868c465e7
jernejk <jernej.kavka@windowslive.com>
2015-07-27 18:18:37 +0200

    Added simple "" to string.Empty code refactor sample.

---
+++ src/EmtpyStringAnalyzer/EmtpyStringAnalyzer/EmtpyStringAnalyzer.Test/UnitTests.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestHelper;
+using EmtpyStringAnalyzer;
+
+namespace EmtpyStringAnalyzer.Test
+{
+    [TestClass]
+    public class UnitTest : CodeFixVerifier
+    {
+
+        //No diagnostics expected to show up
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var test = @"";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public readonly string ReadOnlyEmptyString = """";
+            public const string ConstEmptyString = """";
+            
+            public string Test(string text = """")
+            {
+                string test = """";
+                
+                Console.WriteLine("""");
+
+                switch("""")
+                {
+                    case """":
+                        break;
+                }
+                
+                return """";
+            }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = EmtpyStringAnalyzerAnalyzer.DiagnosticId,
+                Message = "This is not correct empty string!",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 13, 58)
+                        }
+            };
+
+            var expected1 = new DiagnosticResult
+            {
+                Id = EmtpyStringAnalyzerAnalyzer.DiagnosticId,
+                Message = "This is not correct empty string!",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 18, 31)
+                        }
+            };
+
+            var expected2 = new DiagnosticResult
+            {
+                Id = EmtpyStringAnalyzerAnalyzer.DiagnosticId,
+                Message = "This is not correct empty string!",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 20, 35)
+                        }
+            };
+
+            var expected3 = new DiagnosticResult
+            {
+                Id = EmtpyStringAnalyzerAnalyzer.DiagnosticId,
+                Message = "This is not correct empty string!",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 22, 24)
+                        }
+            };
+
+            var expected4 = new DiagnosticResult
+            {
+                Id = EmtpyStringAnalyzerAnalyzer.DiagnosticId,
+                Message = "This is not correct empty string!",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 28, 24)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected, expected1, expected2, expected3, expected4);
+
+            var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public readonly string ReadOnlyEmptyString = string.Empty;
+            public const string ConstEmptyString = """";
+            
+            public string Test(string text = """")
+            {
+                string test = string.Empty;
+                
+                Console.WriteLine(string.Empty);
+
+                switch(string.Empty)
+                {
+                    case """":
+                        break;
+                }
+                
+                return string.Empty;
+            }
+        }
+    }";
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new EmtpyStringAnalyzerCodeFixProvider();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new EmtpyStringAnalyzerAnalyzer();
+        }
+    }
+}
---
+++ src/EmtpyStringAnalyzer/EmtpyStringAnalyzer/EmtpyStringAnalyzer/CodeFixProvider.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+
+using SF = Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace EmtpyStringAnalyzer
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(EmtpyStringAnalyzerCodeFixProvider)), Shared]
+    public class EmtpyStringAnalyzerCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(EmtpyStringAnalyzerAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            foreach (var diagnostic in context.Diagnostics)
+            {
+                // NOTE: Needs getInnermostNodeForTie because top most token can be argument or similar.
+                SyntaxNode node = root.FindNode(diagnostic.Location.SourceSpan, getInnermostNodeForTie: true);
+
+                // Register a code action that will invoke the fix.
+                context.RegisterCodeFix(
+                    CodeAction.Create("Make string.Empty", c => ReplaceEmptyStringWithStringEmpty(context.Document, node, c)),
+                    diagnostic);
+            }
+        }
+
+        private async Task<Document> ReplaceEmptyStringWithStringEmpty(Document document, SyntaxNode oldNode, CancellationToken cancellationToken)
+        {
+            // NOTE: string.Empty
+            var stringType = SF.PredefinedType(SF.Token(SyntaxKind.StringKeyword));
+            var empty = SF.IdentifierName("Empty");
+            var stringEmpty = SF.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, stringType, SF.Token(SyntaxKind.DotToken), empty);
+
+            var tree = await document.GetSyntaxTreeAsync();
+            var node = tree.GetRoot().ReplaceNode(oldNode, stringEmpty);
+
+            return document.WithSyntaxRoot(node);
+        }
+    }
+}
